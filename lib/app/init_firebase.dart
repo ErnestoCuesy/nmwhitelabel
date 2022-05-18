a175bfb7e9d449e848604a9c85cc077b6ec8f783
ernestocuesy <ercuesy@gmail.com>
2022-05-18 12:06:02 +0200

    Firebase initialization now requires default options (generated automatically by Firebase CLI)

--- lib/app/init_firebase.dart
+++ lib/app/init_firebase.dart
@@ -2,9 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nmwhitelabel/app/app.dart';
 import 'package:nmwhitelabel/app/common_widgets/platform_progress_indicator.dart';
+import '../firebase_options.dart';
 
 class InitFirebase extends StatelessWidget {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
 
   @override
   Widget build(BuildContext context) {
